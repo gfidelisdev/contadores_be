bbddd48d637922409c107958e4a800d0ecf80f61
Guilherme Fidelis da Silva <gfidelisdev@gmail.com>
2022-12-22 15:46:40 -0300

    improvements for list counters

--- counters copy.js
+++ counters copy.js
@@ -2,53 +2,33 @@ const knex = require("./database/database")
 
 const Counters = {
   list: async (req, res) => {
-    // Busca os dados de ano e mês para a checagem
+    console.log(req.body)
     year = parseInt(req.body.year)
     month = parseInt(req.body.month)
     initialYear = req.body.year
     finalYear = month + 1 == 13 ? `${year + 1}` : initialYear
     initialMonth = `0${month}`.slice(-2)
     finalMonth = month + 1 == 13 ? `01` : `0${month + 1}`.slice(-2)
+    console.log(finalMonth)
     let printers = await knex("printers")
     printers = printers.map((p) => ({ ...p }))
+    console.log(printers)
 
     let counters = await Promise.all(
       printers.map(async (printer) => {
-        let startCounters = await knex("counters")
+        let countersFromPrinter = await knex("counters")
           .where("printer_id", printer.id)
           .andWhere(
             "created_at",
-            ">",
+            ">=",
             `${initialYear}-${initialMonth}-01 00:00:00`
           )
-          .first()
-        if (!startCounters) {
-          return {
-            msg: `Não há dados para o período informado para a impressora com SN: ${printer["sn"]} e IP: ${printer["ip"]}`,
-          }
-        }
-        let endCounters = await knex("counters")
-          .where("printer_id", printer.id)
-          .andWhere("created_at", ">", `${finalYear}-${finalMonth}-01 00:00:00`)
-          .first()
-
-        if (!endCounters) {
-          endCounters = await knex("counters")
-            .where("printer_id", printer.id)
-            .andWhere(
-              "created_at",
-              ">",
-              `${initialYear}-${initialMonth}-01 00:00:00`
-            )
-            .andWhere(
-              "created_at",
-              "<=",
-              `${finalYear}-${finalMonth}-01 00:00:00`
-            )
-            .orderBy("created_at", "desc")
-
-          endCounters = endCounters.map((eC) => ({ ...eC }))[0]
-        }
+          .andWhere("created_at", "<", `${finalYear}-${finalMonth}-01 00:00:00`)
+        let startCounters = countersFromPrinter[0]
+        let endCounters = countersFromPrinter[countersFromPrinter.length - 1]
+        console.warn(startCounters)
+        //startCounters = startCounters.map(sC=>({...sC}))
+        //endCounters = endCounters.map(eC=>({...eC}))
         totalPrints =
           endCounters["total_prints"] - startCounters["total_prints"]
         totalCopies =
@@ -64,6 +44,26 @@ const Counters = {
         }
       })
     )
+
+    console.log(counters)
+
+    let result = await knex("counters").where(function () {
+      this.where(
+        "created_at",
+        ">=",
+        `${initialYear}-${initialMonth}-01 00:00:00`
+      ).andWhere("created_at", "<", `${finalYear}-${finalMonth}-01 00:00:00`)
+    })
+    let startCounters = await knex("counters")
+      .where("created_at", ">=", `${initialYear}-${initialMonth}-01 00:00:00`)
+      .first()
+    let endCounters = await knex("counters")
+      .where("created_at", "<=", `${finalYear}-${finalMonth}-01 00:00:00`)
+      .first()
+
+    console.log(`${initialYear}-${initialMonth}-01 00:00:00`)
+    console.log(`${finalYear}-${finalMonth}-01 00:00:00`)
+    // console.log(result)
     res.json(counters)
   },
 }
