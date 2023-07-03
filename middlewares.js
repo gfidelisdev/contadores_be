d230a609e69fa8db6ecafcf81b01b81610e34147
Guilherme <guilherme.silva@mpt.mp.br>
2023-07-03 10:38:24 -0300

    20230703.status send on failures list

--- middlewares.js
+++ middlewares.js
@@ -1,13 +1,21 @@
-const ping = require("ping")
+const ping = require("ping");
 const getPrinterStatus = async (printer_ip) => {
-    res = await ping.promise().probe(printer_ip, { timeout: 1 })
-    return res
-}
+    console.log(
+        "🚀 ~ file: middlewares.js:3 ~ getPrinterStatus ~ printer_ip:",
+        printer_ip
+    );
+    let res = await ping.promise.probe(printer_ip, { timeout: 1 });
+    console.log(
+        "🚀 ~ file: middlewares.js:4 ~ getPrinterStatus ~ res:",
+        res.alive
+    );
+    return res.alive;
+};
 
 const getPrintersStatus = async (req, res, next) => {
-    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ req:", req)
-    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ res:", res)
-    next()
-}
+    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ req:", req);
+    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ res:", res);
+    next();
+};
 
-module.exports = { getPrinterStatus, getPrintersStatus }
+module.exports = { getPrinterStatus, getPrintersStatus };
