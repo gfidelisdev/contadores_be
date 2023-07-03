1462e84ec0a8ad9c96c2d0654d99dd2a42ad800f
gfidelisdev <109247627+gfidelisdev@users.noreply.github.com>
2023-07-03 08:56:51 -0300

    20230703.clean code

--- routes.js
+++ routes.js
@@ -1,12 +1,11 @@
 const Counters = require("./counters")
 const Printers = require("./printers")
 const Failures = require("./failures")
-const { getPrintersStatus } = require("./middlewares")
 const route = (app) => {
     app.post("/api/bymonth", Counters.list)
     app.post("/api/byinterval", Counters.filter)
     app.get("/api/printers", Printers.list)
-    app.post("/api/failures", getPrintersStatus, Failures.filter)
+    app.post("/api/failures", Failures.filter)
 }
 
 module.exports = route
