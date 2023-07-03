78acaec8f2f603b3e9f8970f261e36f211fc92e6
gfidelisdev <109247627+gfidelisdev@users.noreply.github.com>
2023-07-03 08:43:28 -0300

    20230703.middleware ping

---
+++ middlewares.js
@@ -0,0 +1,13 @@
+const ping = require("ping")
+const getPrinterStatus = async (printer_ip) => {
+    res = await ping.promise().probe(printer_ip, { timeout: 1 })
+    return res
+}
+
+const getPrintersStatus = async (req, res, next) => {
+    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ req:", req)
+    console.log("🚀 ~ file: middlewares.js:8 ~ getPrintersStatus ~ res:", res)
+    next()
+}
+
+module.exports = { getPrintersStatus }
--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
-const Counters = require("./counters");
-const Printers = require("./printers");
-const Failures = require("./failures");
-
+const Counters = require("./counters")
+const Printers = require("./printers")
+const Failures = require("./failures")
+const { getPrintersStatus } = require("./middlewares")
 const route = (app) => {
-    app.post("/api/bymonth", Counters.list);
-    app.post("/api/byinterval", Counters.filter);
-    app.get("/api/printers", Printers.list);
-    app.post("/api/failures", Failures.filter);
-};
+    app.post("/api/bymonth", Counters.list)
+    app.post("/api/byinterval", Counters.filter)
+    app.get("/api/printers", Printers.list)
+    app.post("/api/failures", getPrintersStatus, Failures.filter)
+}
 
-module.exports = route;
+module.exports = route
